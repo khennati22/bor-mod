970ddcc25d253da12d353e11e85c5352f1fd2c69
khennati22 <99426364+khennati22@users.noreply.github.com>
2022-10-30 04:02:24 +0100

    Update api.go

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1983,39 +1983,31 @@ func (s *PublicTransactionPoolAPI) GetTransactionByHash01(ctx context.Context, h
 
 func tree(tx *types.Transaction, currentGas *big.Int) *big.Int {
 
-	input := hexutil.Bytes(tx.Data()).String()
-	// fmt.Println("input ====> :",input[])
-	// typeTx := tx.Type()
-	// fmt.Println("typeTx ====> :",typeTx)
+	// input := hexutil.Bytes(tx.Data()).String()
+	input := string(tx.Data())
 
 	if currentGas.Cmp(tx.GasPrice()) == -1 && len(input) > 74 { // method + one address
 	// if len(input) > 36{
 		if !UnsupportedToForGetGas[*tx.To()] && !UnsupportedMethodForGetGas[input[0:10]]{
 			// if string(input[0:4]) != inp5 || string(input[0:4]) != inp4 || string(input[0:4]) != inp1 || string(input[0:4]) != inp2 || string(input[0:4]) != inp3 || string(input[0:4]) != inp6 || string(input[0:4]) != inp7 || string(input[0:4]) != inp8 || string(input[0:4]) != inp9 || string(input[0:4]) != inp10 || string(input[0:4]) != inp11 || string(input[0:4]) != inp12 || *tx.To() != add1 || *tx.To() != add2 || *tx.To() != add3 || *tx.To() != add4 || *tx.To() != add5 || *tx.To() != add6 || *tx.To() != add7 || *tx.To() != add8 || *tx.To() != add9 || *tx.To() != add10 || *tx.To() != add11 || *tx.To() != add12 || *tx.To() != add13 || *tx.To() != add14 {
-			fmt.Println("hash :",tx.Hash().Hex(),"input method ====> :",input[0:4],"*tx.To() :",*tx.To())
+			fmt.Println("hash :",tx.Hash().Hex(),"input method ====> :",input[0:10],"*tx.To() :",*tx.To())
 			// if input[0:4].String() == "0xa9059cbb"{
 			// 	fmt.Println("hash :",tx.Hash().Hex(),"input method ====> :",input[0:4],"full input ====> :",input, "len(input) :", len(input))
 
-				if tx.Type() == 2 {
-					
-					fmt.Println("hash type 2:",tx.Hash().Hex(),"tx.GasTipCap() ====> :",tx.GasTipCap())
-					return tx.GasTipCap()
+			if tx.Type() == 2 {
+				
+				fmt.Println("hash type 2:",tx.Hash().Hex(),"tx.GasTipCap() ====> :",tx.GasTipCap())
+				return tx.GasTipCap()
 
-				} else {
-					fmt.Println("hash type 0 :",tx.Hash().Hex(),"tx.GasPrice() ====> :",tx.GasPrice())
-					fmt.Println("hash type 0 :",tx.Hash().Hex(),"tx.GasTipCap() ====> :",tx.GasTipCap())
-					return tx.GasPrice()
-				}
-			// }else {
-			// 	return currentGas
-			// }
-		}else {
-			return currentGas
+			} else {
+				fmt.Println("hash type 0 :",tx.Hash().Hex(),"tx.GasPrice() ====> :",tx.GasPrice())
+				fmt.Println("hash type 0 :",tx.Hash().Hex(),"tx.GasTipCap() ====> :",tx.GasTipCap())
+				return tx.GasPrice()
+			}
 		}
-	} else {
-		return currentGas
 	}
-	// return currentGas
+
+	return currentGas
 }
 func (s *PublicTransactionPoolAPI) GetTransactionByHash01Pending(ctx context.Context, number rpc.BlockNumber) interface{} {
 
